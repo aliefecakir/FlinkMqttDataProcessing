c3da03807cc2d1fa2f059653c3a6604aa897cd36
Ali Efe Çakır <aliefecakir8@gmail.com>
2025-08-25 16:03:42 +0300

    ValueState applied to main function.

--- src/main/java/MqttFlinkProject/Flink/FlinkDataStream.java
+++ src/main/java/MqttFlinkProject/Flink/FlinkDataStream.java
@@ -1,10 +1,15 @@
 package MqttFlinkProject.Flink;
 import MqttFlinkProject.Connector.MqttSource;
 import MqttFlinkProject.Connector.MqttSink;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import java.io.IOException;
 
 public class FlinkDataStream {
 
@@ -20,22 +25,44 @@ public class FlinkDataStream {
 
         DataStream<String> mqttStream = env.addSource(new MqttSource(broker, sourceTopic, sourceClientId));
 
-        DataStream<String> processedStream = mqttStream.map(jsonString -> {
-            ObjectMapper objMapper = new ObjectMapper();
-            JsonNode jsonNode = objMapper.readTree(jsonString);
+        DataStream<String> processedStream = mqttStream
+                .keyBy(jsonString -> {
+                    ObjectMapper objMapper = new ObjectMapper();
+                    JsonNode jsonNode = objMapper.readTree(jsonString);
+                    return jsonNode.get("sensorId").asText();
+                })
+                        .process(new KeyedProcessFunction<>(){
+                            private transient ValueState<Double> lastValueState;
 
-            double value = jsonNode.get("value").asDouble();
-            double newValue = (value*1.8) + 32;
+                            @Override
+                            public void open(org.apache.flink.configuration.Configuration parameters){
+                                ValueStateDescriptor<Double> descriptor =
+                                        new ValueStateDescriptor<>("lastValue", Double.class);
+                                lastValueState = getRuntimeContext().getState(descriptor);
+                            }
 
-            return String.format("{\"sensorId\": %s, \"valueFahrenheit\": %.1f}",
-                                                jsonNode.get("sensorId").asText(), newValue);
-        });
+                            @Override
+                            public void processElement(String jsonString, Context ctx, Collector<String> out) throws IOException {
+                                ObjectMapper objMapper = new ObjectMapper();
+                                JsonNode jsonNode = objMapper.readTree(jsonString);
+
+                                double value = jsonNode.get("value").asDouble();
+                                double newValue = (value*1.8) + 32;
+
+                                Double lastValue = lastValueState.value();
+
+                                if(lastValue == null || !lastValue.equals(newValue)) {
+                                    lastValueState.update(newValue);
+                                    out.collect(String.format("{\"sensorId\": \"%s\", \"valueFahrenheit\": %.1f}",
+                                            jsonNode.get("sensorId").asText(), newValue));
+                                }
+                            }
+                        });
 
         processedStream.addSink(new MqttSink(broker, sinkTopic, sinkClientId)).setParallelism(1);
 
         System.out.println("Flink job is running. Listening on '" + sourceTopic + "' and publishing to '" + sinkTopic + "'...");
         env.execute();
-
     }
 
 }
