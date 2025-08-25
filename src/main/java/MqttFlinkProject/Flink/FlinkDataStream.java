cf66981848dac6bd9a5d351f6c287bbabedd326c
Ali Efe Çakır <aliefecakir8@gmail.com>
2025-08-25 12:52:31 +0300

    main function modified for JSON input/output

--- src/main/java/MqttFlinkProject/Flink/FlinkDataStream.java
+++ src/main/java/MqttFlinkProject/Flink/FlinkDataStream.java
@@ -1,6 +1,8 @@
 package MqttFlinkProject.Flink;
 import MqttFlinkProject.Connector.MqttSource;
 import MqttFlinkProject.Connector.MqttSink;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -11,13 +13,25 @@ public class FlinkDataStream {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         String broker = "tcp://localhost:1883";
-        String sourceTopic = "test/flink/source";
-        String sinkTopic = "test/flink/sink";
+        String sourceTopic = "sensor/input/temp";
+        String sinkTopic = "sensor/output/temp";
         String sourceClientId = "flink-client-source";
         String sinkClientId = "flink-client-sink";
 
         DataStream<String> mqttStream = env.addSource(new MqttSource(broker, sourceTopic, sourceClientId));
-        mqttStream.addSink(new MqttSink(broker, sinkTopic, sinkClientId)).setParallelism(1);
+
+        DataStream<String> processedStream = mqttStream.map(jsonString -> {
+            ObjectMapper objMapper = new ObjectMapper();
+            JsonNode jsonNode = objMapper.readTree(jsonString);
+
+            double value = jsonNode.get("value").asDouble();
+            double newValue = (value*1.8) + 32;
+
+            return String.format("{\"sensorId\": %s, \"valueFahrenheit\": %.1f}",
+                                                jsonNode.get("sensorId").asText(), newValue);
+        });
+
+        processedStream.addSink(new MqttSink(broker, sinkTopic, sinkClientId)).setParallelism(1);
 
         System.out.println("Flink job is running. Listening on '" + sourceTopic + "' and publishing to '" + sinkTopic + "'...");
         env.execute();
